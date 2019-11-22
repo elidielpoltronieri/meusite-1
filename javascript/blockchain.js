506ee45d2192982a07452e784f32f6b550ee98ac
elidielpoltronieri <56139039+elidielpoltronieri@users.noreply.github.com>
2019-11-21 21:06:19 -0300

    Add files via upload

--- javascript/blockchain.js
+++ javascript/blockchain.js
@@ -1,4 +1,4 @@
-var enderecoContrato = "0x53C34f4897a4111f9299a9b734aD1d5c380a8Ce5";
+var enderecoContrato = "0xd406234a708A7B11787d80b227ab3FAA7F975311";
 var provedor = new ethers.providers.Web3Provider(web3.currentProvider);
 ethereum.enable();
 var signatario = provedor.getSigner();
